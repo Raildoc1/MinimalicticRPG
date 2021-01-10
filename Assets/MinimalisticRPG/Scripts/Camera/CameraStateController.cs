07212887b4321f730de89378a9a7bc13bef3598d
Raildoc1 <Kirill1056@list.ru>
2021-01-10 16:20:36 +0700

    Inventory camera

--- Assets/MinimalisticRPG/Scripts/Camera/CameraStateController.cs
+++ Assets/MinimalisticRPG/Scripts/Camera/CameraStateController.cs
@@ -10,14 +10,12 @@ namespace KG.CameraControl
         [SerializeField] private GameObject freeCamera;
         [SerializeField] private GameObject lockCamera;
         [SerializeField] private GameObject dialogCamera;
+        [SerializeField] private GameObject inventoryCamera;
 
         [SerializeField] private Transform playerTransform;
         [SerializeField] private Transform cameraTransform;
 
         private CinemachineFreeLook freeLookCamera;
-        private Vector3 cameraDefaultForward;
-        private Vector3 cameraDefaultRight;
-        private Vector3 cameraDefaultUp;
 
         private StateSwitch playerStateSwtich;
 
@@ -25,7 +23,7 @@ namespace KG.CameraControl
         {
             freeLookCamera = freeCamera.GetComponent<CinemachineFreeLook>();
 
-            if(!freeLookCamera)
+            if (!freeLookCamera)
             {
                 Debug.LogError("No CinemachineFreeLook component on freeCamera");
             }
@@ -45,9 +43,6 @@ namespace KG.CameraControl
 
             freeLookCamera.m_XAxis.Value = 0f;
             freeLookCamera.m_YAxis.Value = 0f;
-            cameraDefaultForward = cameraTransform.forward;
-            cameraDefaultRight = cameraTransform.right;
-            cameraDefaultUp = cameraTransform.up;
         }
 
         private void OnDisable()
@@ -55,21 +50,12 @@ namespace KG.CameraControl
             playerStateSwtich.onStateChange.RemoveListener(OnChangeState);
         }
 
-        private void FixedUpdate()
-        {
-            var fwd = cameraTransform.forward;
-            var tmp = new Vector3(fwd.x, 0f, fwd.z);
-            //Debug.Log($"angle (w/o y) = {Vector3.Angle(cameraDefaultForward, tmp)} {Vector3.Angle(cameraDefaultRight, tmp)}");
-            //Debug.Log($"angle (proj w/o y) = {Vector3.Angle(Vector3.ProjectOnPlane(cameraDefaultForward, cameraDefaultUp), tmp)} {Vector3.Angle(Vector3.ProjectOnPlane(cameraDefaultForward, cameraDefaultUp), tmp)}");
-            //Debug.Log($"angle (proj) = {Vector3.Angle(Vector3.ProjectOnPlane(cameraDefaultForward, cameraDefaultUp), fwd)} {Vector3.Angle(Vector3.ProjectOnPlane(cameraDefaultForward, cameraDefaultUp), fwd)}");
-            //Debug.Log($"angle = {Vector3.Angle(cameraDefaultForward, fwd)} {Vector3.Angle(cameraDefaultRight, fwd)}");
-        }
-
         public void LockOnTarget()
         {
             freeCamera.SetActive(false);
             lockCamera.SetActive(true);
             dialogCamera.SetActive(false);
+            inventoryCamera.SetActive(false);
         }
 
         public void FreeCamera()
@@ -77,21 +63,7 @@ namespace KG.CameraControl
             freeCamera.SetActive(true);
             lockCamera.SetActive(false);
             dialogCamera.SetActive(false);
-
-
-            //freeLookCamera.transform.position = cameraTransform.position;
-            //freeLookCamera.ForceCameraPosition(cameraTransform.position, Quaternion.Euler(Vector3.zero));
-
-            //var forward = cameraTransform.forward;
-            //var tmp = new Vector3(forward.x, 0f, forward.z);
-
-            ////var targetAngle = Vector3.Angle(-Vector3.forward, tmp);
-            //var targetAngle = Vector3.Angle(cameraDefaultForward, tmp);
-            ////float rightAngle = Vector3.Angle(Vector3.right, tmp);
-            //float rightAngle = Vector3.Angle(cameraDefaultForward, tmp);
-
-            //freeLookCamera.m_XAxis.Value = rightAngle > 90f ? -targetAngle : targetAngle;
-            //freeLookCamera.m_YAxis.Value = 0.5f;
+            inventoryCamera.SetActive(false);
         }
 
         public void StartDialog()
@@ -99,6 +71,15 @@ namespace KG.CameraControl
             freeCamera.SetActive(false);
             lockCamera.SetActive(false);
             dialogCamera.SetActive(true);
+            inventoryCamera.SetActive(false);
+        }
+
+        public void OpenInventory()
+        {
+            freeCamera.SetActive(false);
+            lockCamera.SetActive(false);
+            dialogCamera.SetActive(false);
+            inventoryCamera.SetActive(true);
         }
 
         public void OnChangeState(State _, State currentState)
@@ -107,8 +88,13 @@ namespace KG.CameraControl
             {
                 StartDialog();
             }
-            else if(currentState == State.PEACE) {
+            else if (currentState == State.PEACE)
+            {
                 FreeCamera();
+            }
+            else if (currentState == State.INVENTORY)
+            {
+                OpenInventory();
             }
         }
     }
